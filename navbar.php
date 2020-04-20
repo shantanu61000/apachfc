675e021c9494d909e1fe99160cb0cd27a1a884ff
Shantanu Tripathi <shantanutripathi96@gmail.com>
2020-04-21 01:57:51 +0530

    2oth april

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>       
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="navstyle.css">
+        <script src="https://code.jquery.com/jquery-3.4.1.js"  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+</head>
+    <body>
+
+        <div class="container-fluid">    
+            
+<!-------------navbar starts------------>
+                <nav class="navbar navbar-expand-lg navbar-light ">
+                    <div class="container">
+                        <a class="navbar-brand" href="#"> <img src="img/APAClogo2.png" class="img-logo"> </a>
+                        <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarResponse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>                            
+                            <div class="collapse navbar-collapse" style="transition: 1s;" id="navbarResponse" >
+                                <ul class="navbar-nav m-auto ">
+                                    <li class="nav-item"><a class="nav-link" href="#home">INTRO</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#loan">LOAN</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#contactus">CONTACT US</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#TAB4">TAB4</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#TAB5">TAB5</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#TAB6">TAB6</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#TAB7">TAB7</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#TAB8">TAB8</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                </nav>
+<!-------------navbar ends------------>
+        </div>
+
+    </body>
+</html>
